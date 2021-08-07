d26714cb20669651c1fca2273b80f6b380742903
John Allen Florencio <74499662+johnallen23@users.noreply.github.com>
2021-08-07 23:50:58 +0800

    Add files via upload

---
+++ assets/js/custom.js
@@ -0,0 +1,108 @@
+(function ($) {
+
+	"use strict";
+
+
+	$('.owl-carousel').owlCarousel({
+		loop: true,
+		margin: 30,
+		nav: true,
+		pagination: true,
+		responsive: {
+			0: {
+				items: 1
+			},
+			600: {
+				items: 1
+			},
+			1000: {
+				items: 2
+			}
+		}
+	})
+
+
+	$(window).scroll(function () {
+		var scroll = $(window).scrollTop();
+		var box = $('.header-text').height();
+		var header = $('header').height();
+
+		if (scroll >= box - header) {
+			$("header").addClass("background-header");
+		} else {
+			$("header").removeClass("background-header");
+		}
+	});
+
+	// Window Resize Mobile Menu Fix
+	mobileNav();
+
+
+	// Scroll animation init
+	window.sr = new scrollReveal();
+
+
+	// Menu Dropdown Toggle
+	if ($('.menu-trigger').length) {
+		$(".menu-trigger").on('click', function () {
+			$(this).toggleClass('active');
+			$('.header-area .nav').slideToggle(200);
+		});
+	}
+
+
+	
+
+	// Home seperator
+	if ($('.home-seperator').length) {
+		$('.home-seperator .left-item, .home-seperator .right-item').imgfix();
+	}
+
+
+	// Home number counterup
+	if ($('.count-item').length) {
+		$('.count-item strong').counterUp({
+			delay: 10,
+			time: 1000
+		});
+	}
+
+
+	// Page loading animation
+	$(window).on('load', function () {
+		if ($('.cover').length) {
+			$('.cover').parallax({
+				imageSrc: $('.cover').data('image'),
+				zIndex: '1'
+			});
+		}
+
+		$("#preloader").animate({
+			'opacity': '0'
+		}, 600, function () {
+			setTimeout(function () {
+				$("#preloader").css("visibility", "hidden").fadeOut();
+			}, 300);
+		});
+	});
+
+
+	// Window Resize Mobile Menu Fix
+	$(window).on('resize', function () {
+		mobileNav();
+	});
+
+
+	// Window Resize Mobile Menu Fix
+	function mobileNav() {
+		var width = $(window).width();
+		$('.submenu').on('click', function () {
+			if (width < 992) {
+				$('.submenu ul').removeClass('active');
+				$(this).find('ul').toggleClass('active');
+			}
+		});
+	}
+
+
+})(window.jQuery);
